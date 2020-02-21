01227f5c3d3e94fd186c51a801f73c0e7d43f767
evildark <evildark0507@gmail.com>
2020-02-21 12:36:40 +0800

    update localization.tw.lua v1.1
    update some event can't work on zh-TW client duo to strings does not match
    Nefarian class call
    Chromaggus spell weakness

--- DBM-BWL/localization.tw.lua
+++ DBM-BWL/localization.tw.lua
@@ -1,4 +1,4 @@
-﻿if GetLocale() ~= "zhTW" then return end
+if GetLocale() ~= "zhTW" then return end
 local L
 
 -----------------
@@ -17,7 +17,7 @@ L:SetOptionLocalization{
 }
 L:SetMiscLocalization{
 	Phase2Emote	= "在寶珠的控制力消失之前逃走。",
-	YellPull 	= "入侵者闖入孵化室了!警報!不惜一切代價保護蛋!"
+	YellPull 	= "入侵者闖進孵化室了！拉響警報！無論如何都要保護蛋！"
 }
 -------------------
 --  Vaelastrasz  --
@@ -29,7 +29,7 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	Event	= "太遲了，朋友! 奈法利斯的腐化已經掌握了我...我已經無法...控制我自己了。"
+	Event	= "太遲了，朋友"
 }
 -----------------
 --  Broodlord  --
@@ -41,7 +41,7 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	Pull	= "你怎麼進來的?你們這種生物不能進來!我要毀滅你們!"
+	Pull	= "你們根本不應該在這裡出現！我要殺掉你們！"
 }
 
 ---------------
@@ -83,15 +83,20 @@ L:SetWarningLocalization{
 	WarnBreath		= "%s"
 }
 L:SetTimerLocalization{
-	TimerBreathCD	= "%s冷卻"
+	TimerBreathCD	= "%s 冷卻",
+	TimerBreath		= "%s 施放",
+	TimerVulnCD		= "弱點冷卻"
 }
 L:SetOptionLocalization{
 	WarnBreath		= "為克洛瑪古斯其中一個吐息顯示警告",
-	TimerBreathCD	= "顯示吐息冷卻"
+	TimerBreathCD	= "顯示吐息冷卻",
+	TimerBreath		= "顯示吐息施放",
+	TimerVulnCD		= "顯示弱點冷卻"
 }
 L:SetMiscLocalization{
 	Breath1	= "第一次吐息",
-	Breath2	= "第二次吐息"
+	Breath2	= "第二次吐息",
+	VulnEmote	= "因皮膚閃著微光而驚訝退縮"
 }
 
 ----------------
@@ -103,27 +108,30 @@ L:SetGeneralLocalization{
 	name = "奈法利安"
 }
 L:SetWarningLocalization{
-	WarnClassCall		= "%s呼喊"
+	WarnAddsLeft		= "剩下 %d 擊殺",
+	WarnClassCall		= "%s 點名"
 }
 L:SetTimerLocalization{
-	TimerClassCall		= "%s呼喊結束"
+	TimerClassCall		= "%s 點名結束"
 }
 L:SetOptionLocalization{
-	TimerClassCall		= "為職業呼喊持續時間顯示計時器",
-	WarnClassCall		= "提示職業呼喊"
+	TimerClassCall		= "為職業點名持續時間顯示計時器",
+	WarnAddsLeft		= "提示離第二階段開始剩多少擊殺",
+	WarnClassCall		= "提示職業點名"
 }
 L:SetMiscLocalization{
-	YellP2		= "幹得好，我的手下。凡人的勇氣開始消退!現在，現在讓我們看看他們如何應對黑石之王的力量!!!",
-	YellP3		= "不可能!出現吧，我的僕人!再次為我的主人服務!",
-	YellShaman	= "薩滿，讓我看看",
-	YellPaladin	= "聖騎士...聽說你有無數條命。讓我看看到底是怎麼樣的吧。",
-	YellDruid	= "德魯伊和你們愚蠢的變身術。讓我們看看什麼會發生吧!",
-	YellPriest	= "牧師!如果你要繼續這麼治療的話，那我們來玩點有趣的東西!",
-	YellWarrior	= "戰士，我知道你的力量不只如此!讓我們來見識一下吧!",
-	YellRogue	= "盜賊?不要躲了，面對我吧!",
-	YellWarlock	= "術士，不要隨便去玩那些你不理解的法術。看看會發生什麼吧?",
-	YellHunter	= "獵人和你那討厭的豌豆射擊!",
-	YellMage	= "還有法師?你應該小心使用你的魔法...",
-	YellDK		= "死亡騎士們...來這。",
+	YellP1		= "讓賽事開始吧！",
+	YellP2		= "幹得好",
+	YellP3		= "不可能",
+	YellShaman	= "薩滿",
+	YellPaladin	= "聖騎士",
+	YellDruid	= "德魯伊",
+	YellPriest	= "牧師",
+	YellWarrior	= "戰士",
+	YellRogue	= "盜賊",
+	YellWarlock	= "術士",
+	YellHunter	= "獵人",
+	YellMage	= "法師",
+	YellDK		= "死亡騎士",
 	YellMonk	= "武僧"
 }
