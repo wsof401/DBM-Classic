69b52e5114d8b75c2cfd3084cb2ac7fb5be28953
woopydalan <29320668+woopydalan@users.noreply.github.com>
2020-04-01 01:22:45 +0000

    Update localization.es.lua

--- DBM-Party-Classic/localization.es.lua
+++ DBM-Party-Classic/localization.es.lua
@@ -1478,7 +1478,7 @@ L:SetGeneralLocalization{
 L = DBM:GetModLocalization(485)
 
 L:SetGeneralLocalization{
-	name 		= "Theka el Mártir"
+	name 		= "Theka la Mártir"
 }
 
 -----------------------------
